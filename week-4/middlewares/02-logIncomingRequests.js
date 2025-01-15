ec255bdd0c3059a4da9d6306d5ba3093358fcc83
Varun Arora <aroravaruncode@gmail.com>
2025-01-15 18:53:38 +0530

    complete week-4 middlewares

--- week-4/middlewares/02-logIncomingRequests.js
+++ week-4/middlewares/02-logIncomingRequests.js
@@ -5,6 +5,8 @@ const app = express();
 
 function logRequests(req, res, next) {
     // write the logic for request log here
+    console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
+    next()
 }
 
 app.use(logRequests);
@@ -12,5 +14,7 @@ app.use(logRequests);
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello, world!' });
 });
+
+app.listen(3000)
 
 module.exports = app;
